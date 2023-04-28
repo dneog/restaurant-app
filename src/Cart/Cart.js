2e9a324203ee91889fadbe701ba6c2dbb4857611
dneog <debashisneog23@gmail.com>
2023-04-28 09:29:36 +0530

    Enable the delete button of Cart Items

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -9,12 +9,20 @@ const Cart = (props) => {
     const CartCtx= useContext(CartContext)
     const totalAmount = `$${CartCtx.totalAmount.toFixed(2)}`;
     const checkItems = CartCtx.items.length > 0
+
+    const CartItemRemoveHandler =(id)=> {
+        CartCtx.removeItem(id)
+    }
+    const CartItemAddHandler = (item)=> {
+      CartCtx.addItem({...item, amount: 1})
+    }
     const CartItems= <ul>
      {CartCtx.items.map((item)=>
      <li className='item-Name'>
     <CartItem key={item.id}
     name={item.name}
-      amount= {item.amount} price={item.price}
+      amount= {item.amount} price={item.price} onRemove= {CartItemRemoveHandler.bind(null, item.id)}
+      onAdd= {CartItemAddHandler.bind(null, item)}
     />
      </li>) }
       </ul>
--- src/Cart/CartItem.js
+++ src/Cart/CartItem.js
@@ -4,15 +4,23 @@ const CartItem = (props)=> {
 
 
 return (
-    <li>
+    <li className='border-bottom'>
+        <div className='main'>
         <div className='display'>
             <span className="foodName">{props.name}</span>
           
            <div>
            <span>{price}</span>
+           <span className='nums'> x{props.amount}</span>
            </div>
 
 
+        </div>
+        <div>
+            <button className='minus' onClick={props.onRemove}>-</button>
+            <button className='plus' onClick={props.onAdd}>+</button>
+
+        </div>
         </div>
        
     </li>
